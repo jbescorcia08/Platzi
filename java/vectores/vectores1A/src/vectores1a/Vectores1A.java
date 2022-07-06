57a1eec41b910d835d8e939b721edfca1cc8cdde
JesusBarrios <jbescorcia@outlook.es>
2022-07-05 21:20:56 -0500

    migrando a wn11

---
+++ java/vectores/vectores1A/src/vectores1a/Vectores1A.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package vectores1a;
+
+public class Vectores1A {
+
+    
+    public static void main(String[] args) {
+        
+        int[] numeros = new int[3];
+        
+        numeros[0] = 7;
+        numeros[1] = 10;
+        numeros[2] = 13;
+        
+        for (int i = 0; i <= 2; i++) {
+            System.out.println(numeros[i]);
+        }
+    }
+
+}

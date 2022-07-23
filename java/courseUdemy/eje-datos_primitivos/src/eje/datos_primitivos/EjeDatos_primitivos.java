e6224a63730ceb8aa92cd5ddad9bba6ba94e5582
jesus_Barrios <Jbescorcia@outlook.es>
2022-07-23 10:32:19 -0500

    theme apache

---
+++ java/courseUdemy/eje-datos_primitivos/src/eje/datos_primitivos/EjeDatos_primitivos.java
@@ -0,0 +1,12 @@
+
+package eje.datos_primitivos;
+
+//@author Sistemas
+
+public class EjeDatos_primitivos {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}

42e531c2cc64ad2def33a3a7920dcd744cd71249
jesus_Barrios <Jbescorcia@outlook.es>
2022-07-23 12:20:56 -0500

    theme apache

---
+++ java/courseUdemy/Uso-var-tipoPrimitivo/src/uso/var/tipoprimitivo/UsoVarTipoPrimitivo.java
@@ -0,0 +1,15 @@
+
+package uso.var.tipoprimitivo;
+
+//@author Sistemas
+
+public class UsoVarTipoPrimitivo {
+
+    public static void main(String[] args) {
+        // Uso de la palabra reservada --> var
+        
+        var numeroEntero = 10;
+        System.out.println("Numero entero; "+numeroEntero);
+    }
+
+}
---
+++ java/courseUdemy/eje-primitivosFlotantes/src/eje/primitivosflotantes/EjePrimitivosFlotantes.java
@@ -0,0 +1,27 @@
+
+package eje.primitivosflotantes;
+
+//@author Sistemas
+
+public class EjePrimitivosFlotantes {
+
+    public static void main(String[] args) {
+        // Datos primitivos flotantes "DECIMALES"; float, double
+        
+        //float 
+        float numeroFloat = 10.4F;
+        
+        System.out.println("Numero Flotante; "+numeroFloat);
+        System.out.println("valor minimo float; "+ Float.MIN_VALUE);
+        System.out.println("valor maximo float; "+ Float.MAX_VALUE);
+        
+        System.out.println("---------------------------------------------------");
+        //doble
+        
+        double numeroDouble = 34.1;
+        System.out.println("Numero Double; "+numeroDouble);
+        System.out.println("valor minimo Double; "+ Double.MIN_VALUE);
+        System.out.println("valor maximo Double; "+ Double.MAX_VALUE);
+    }
+
+}

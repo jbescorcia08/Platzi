302ad7324c76895c7df938dafa418916570c3797
jesus_Barrios <Jbescorcia@outlook.es>
2022-10-04 16:58:26 -0500

    java

---
+++ java/ejercicios/matrisConsola/src/matrisconsola/MatrisConsola.java
@@ -0,0 +1,12 @@
+
+package matrisconsola;
+
+//@author Sistemas
+
+public class MatrisConsola {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
--- java/ejercicios/ordenarArreglo/src/ordenararreglo/OrdenarArreglo.java
+++ java/ejercicios/ordenarArreglo/src/ordenararreglo/OrdenarArreglo.java
@@ -7,16 +7,20 @@ public class OrdenarArreglo {
 
     public static void main(String[] args) {
         
-        int[] numeros = {1,8,2,7,3,6,3,4,5,10,9,12,11,13};
+        int[] numeros = {1,8,2,7,3,6,3,4,5,10,9,102,11,13};
         
         
         for (int i = 0; i < numeros.length -1; i++) {
             for (int j = i+1; j < numeros.length; j++) {
                 if(numeros[i] > numeros[j]){
-                    int n = numeros[i];
-                    
+                    int numero = numeros[i];
+                    numeros[i] = numeros[j];
+                    numeros[j] = numero;
                 }
             }
+        }
+        for (int n = 0; n < numeros.length; n++) {
+            System.out.print(" ["+numeros[n]+"] ");
         }
     }
 

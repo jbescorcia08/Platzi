cfd84595b5926a647949ad8019d80d46b9f7308e
jesus_Barrios <Jbescorcia@outlook.es>
2022-12-09 17:11:53 -0500

    oden

---
+++ java/POO/Encapsulamiento/src/test/PersonaPrueba.java
@@ -0,0 +1,11 @@
+package test;
+
+import dominio.*;
+
+public class PersonaPrueba {
+    public static void main(String[] args) {
+        Persona persona = new Persona("jesus",5000,false);
+
+
+    }
+}
--- java/POO/HolaMundo/src/palabraThis/PalabraThis.java
+++ java/POO/HolaMundo/src/palabraThis/PalabraThis.java
@@ -1,5 +1,7 @@
 package palabraThis;
 
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class PalabraThis {
     public static void main(String[] args) {
         Persona persona = new Persona("Jesus", "Barrios");
@@ -11,8 +13,17 @@ class Persona{
     String nombre;
     String apellido;
     Persona(String nombre, String apellido){
+        //super();
         this.nombre = nombre;
         this.apellido = apellido;
+        System.out.println("objeto persona usando this: "+this);
+        new Imprimir().imprimir(this);
     }
 
+}
+class Imprimir{
+    public void imprimir(Persona persona){
+        System.out.println("Persona desde imprimir: "+persona);
+        System.out.println("THis: "+this);
+    }
 }

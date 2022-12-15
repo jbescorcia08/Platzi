13b70987bc7eeafa9e6b99d00d4d0e50a0b8977a
jesus_Barrios <Jbescorcia@outlook.es>
2022-12-15 14:12:30 -0500

    java

--- java/POO/Herencia/src/domain/Cliente.java
+++ java/POO/Herencia/src/domain/Cliente.java
@@ -4,30 +4,44 @@ import java.util.Date;
 
 
 public class Cliente extends Persona{
-    private int idCliente;
-    private boolean vip;
-    private String fechaRegistro;
+    private  int idCliente;
+    private Date fechaRegistro;
     private static int contadorCliente;
-    private static Date date = new Date();
+    private boolean vip;
 
-    public Cliente(String nombre, boolean vip) {
-        super(nombre);
-        this.idCliente = ++Cliente.contadorCliente;
+    public Cliente(Date fechaRegistro, boolean vip, String nombre, char genero, int edad, String direccion){
+        super(nombre,genero,edad,direccion);
         this.vip = vip;
-        this.fechaRegistro = ;
-    }
-    public Cliente(String nombre){
-        this.nombre = nombre;
+        this.fechaRegistro = fechaRegistro;
+        this.idCliente = ++Cliente.contadorCliente;
     }
 
+    public int getIdCliente() {
+        return idCliente;
+    }
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public boolean isVip() {
+        return vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
 
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Cliente{");
-        sb.append("idCliente=").append(this.idCliente);
+        sb.append("idCliente=").append(idCliente);
+        sb.append(", fechaRegistro=").append(fechaRegistro);
+        sb.append(", vip=").append(vip);
         sb.append(", ").append(super.toString());
-        sb.append(", fechaRegistro='").append(FechaCliente()).append('\'');
-        sb.append(", vip=").append(this.vip);
         sb.append('}');
         return sb.toString();
     }

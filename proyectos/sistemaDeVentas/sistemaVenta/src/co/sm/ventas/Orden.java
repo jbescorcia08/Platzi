b3b82c19e084ef3f126fc7bbb0e85530d517261a
Jesus_Barrios <jbescorcia@outlook.es>
2023-01-05 18:03:34 -0500

    java

--- proyectos/sistemaDeVentas/sistemaVenta/src/co/sm/ventas/Orden.java
+++ proyectos/sistemaDeVentas/sistemaVenta/src/co/sm/ventas/Orden.java
@@ -1,10 +1,41 @@
 package co.sm.ventas;
 
-public class Orden {
+import javax.swing.*;
 
-    protected int idOrden;
-    protected String productos [] ;
-    protected int contadorOrdenes = 0;
-    protected final int MAX_PRODUCTOS = 10;
+public class Orden {
+    private int idOrden;
+    private Producto Productos[];
+    private static int contadorOrdenes;
+    private int contadorProductos;
+    private static int MAX_PRODUCTOS = 10;
+
+    public Orden(){
+        this.idOrden = ++Orden.contadorOrdenes;
+        this.Productos = new Producto[Orden.MAX_PRODUCTOS];
+    }
+    public void agregarProducto(Producto producto){
+        if(this.contadorProductos < Orden.MAX_PRODUCTOS){
+            this.Productos[this.contadorProductos++] = producto;
+        }else{
+            JOptionPane.showMessageDialog(null,"Ha superado el limite de produstos: "+Orden.MAX_PRODUCTOS);
+        }
+    }
+    public double calcularTotal(){
+        double total = 0;
+        for (int i = 0; i < this.contadorProductos; i++) {
+            total += this.Productos[i].getPrecio();
+        }
+        return total;
+    }
+    public void mostrarOrden(){
+        System.out.println("id Orden: "+this.idOrden);
+        double totalOrden = this.calcularTotal();
+        System.out.println("Total Orden: $"+totalOrden);
+        System.out.println("Productos de la Orden: ");
+        for (int i = 0; i < this.contadorProductos; i++) {
+            System.out.println(this.Productos[i]);
+        }
+    }
+
 
 }
--- proyectos/sistemaDeVentas/sistemaVenta/src/testVentas/TestVentas.java
+++ proyectos/sistemaDeVentas/sistemaVenta/src/testVentas/TestVentas.java
@@ -1,4 +1,19 @@
 package testVentas;
 
+import co.sm.ventas.*;
+
 public class TestVentas {
+    public static void main(String[] args) {
+        Producto prodducto1 = new Producto("camisa", 400);
+        Producto prodducto2 = new Producto("pantalon", 500);
+        Producto prodducto3 = new Producto("camisa", 400);
+        Producto prodducto4 = new Producto("pantalon", 500);
+
+        Orden orden1 = new Orden();
+        orden1.agregarProducto(prodducto1);
+        orden1.agregarProducto(prodducto2);
+        orden1.agregarProducto(prodducto3);
+        orden1.agregarProducto(prodducto4);
+        orden1.mostrarOrden();
+    }
 }
